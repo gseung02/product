248d19632c00f38a63fb3950da1cdc81a4d75d21
gseung02 <jiseung1102@naver.com>
2025-02-10 14:31:22 +0900

    updatesetting

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,13 +1,13 @@
 export const products = [
-  {id:1, name:'피자', price: 20000, image:'./public/images/pizza.jpg',category:'Best'},
-  {id:2, name:'햄버거', price: 8000, image:'./public/images/burger.jpg',category:'세트'},
-  {id:3, name:'콜라', price: 2000, image:'./public/images/kola.jpg',category:'음료'},
-  {id:4, name:'감자튀김', price: 4000, image:'./public/images/frenchfries.jpg',category:'디저트'},
-  {id:5, name:'밀크쉐이크', price: 3000, image:'./public/images/milkshake.jpg',category:'New'},
-  {id:6, name:'어니언링', price: 3000, image:'./public/images/img-1.png',category:'디저트'},
-  {id:7, name:'사이다', price: 2000, image:'./public/images/img-2.png',category:'음료'},
-  {id:8, name:'에그타르트', price: 3000, image:'./public/images/img-3.png',category:'디저트'},
-  {id:9, name:'오렌지주스', price: 3000, image:'./public/images/img-4.png',category:'음료'},
-  {id:10, name:'치즈스틱', price: 3000, image:'./public/images/img-5.png',category:'Best'}
+  {id:1, name:'피자', price: 20000, image:'./images/pizza.jpg',category:'Best'},
+  {id:2, name:'햄버거', price: 8000, image:'./images/burger.jpg',category:'세트'},
+  {id:3, name:'콜라', price: 2000, image:'./images/kola.jpg',category:'음료'},
+  {id:4, name:'감자튀김', price: 4000, image:'./images/frenchfries.jpg',category:'디저트'},
+  {id:5, name:'밀크쉐이크', price: 3000, image:'./images/milkshake.jpg',category:'New'},
+  {id:6, name:'어니언링', price: 3000, image:'./images/img-1.png',category:'디저트'},
+  {id:7, name:'사이다', price: 2000, image:'./images/img-2.png',category:'음료'},
+  {id:8, name:'에그타르트', price: 3000, image:'./images/img-3.png',category:'디저트'},
+  {id:9, name:'오렌지주스', price: 3000, image:'./images/img-4.png',category:'음료'},
+  {id:10, name:'치즈스틱', price: 3000, image:'./images/img-5.png',category:'Best'},
 ];
 export const categories = ['전체','Best','New','세트','음료','디저트'];
