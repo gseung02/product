d53e33a9aaa207e64f2afc46970fc8f30c8537c1
gseung02 <jiseung1102@naver.com>
2025-02-10 14:45:36 +0900

    update setting

--- src/assets/products.js
+++ src/assets/products.js
@@ -4,10 +4,10 @@ export const products = [
   {id:3, name:'콜라', price: 2000, image:'./images/kola.jpg',category:'음료'},
   {id:4, name:'감자튀김', price: 4000, image:'./images/frenchfries.jpg',category:'디저트'},
   {id:5, name:'밀크쉐이크', price: 3000, image:'./images/milkshake.jpg',category:'New'},
-  {id:6, name:'어니언링', price: 3000, image:'./images/img-1.png',category:'디저트'},
-  {id:7, name:'사이다', price: 2000, image:'./images/img-2.png',category:'음료'},
-  {id:8, name:'에그타르트', price: 3000, image:'./images/img-3.png',category:'디저트'},
-  {id:9, name:'오렌지주스', price: 3000, image:'./images/img-4.png',category:'음료'},
-  {id:10, name:'치즈스틱', price: 3000, image:'./images/img-5.png',category:'Best'},
+  {id:6, name:'어니언링', price: 3000, image:'./images/onionRing.jpg',category:'디저트'},
+  {id:7, name:'레모네이드', price: 2000, image:'./images/remonade.jpg',category:'음료'},
+  {id:8, name:'에그타르트', price: 3000, image:'./images/eggTart.jpg',category:'디저트'},
+  {id:9, name:'오렌지주스', price: 3000, image:'./images/orangeJuice.jpg',category:'음료'},
+  {id:10, name:'샐러드', price: 3000, image:'./images/salad.jpg',category:'Best'},
 ];
 export const categories = ['전체','Best','New','세트','음료','디저트'];
