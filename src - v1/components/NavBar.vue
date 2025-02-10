5ea3578819165192779d849a06820b467c263394
gseung02 <jiseung1102@naver.com>
2025-02-10 12:34:00 +0900

    first commit

---
+++ src - v1/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <h1>맛있게 먹자</h1>
+    <ul>
+      <li v-for="(list,idx) in categories" :key="idx" @click="selectedClick(list)">{{ list}}</li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+defineProps({
+  categories: Array
+});
+const emit = defineEmits(['selected']);
+const selectedClick = (list) => {
+  emit('selected',list);
+}
+</script>
+
+<style lang="scss" scoped>
+  nav{
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem 5rem;
+    ul{
+      display: flex;
+      gap: 3rem;
+      li{
+        cursor: pointer;
+        &:hover{
+          color: blue;
+          font-weight: bolder;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/assets/products.js
@@ -0,0 +1,13 @@
+export const products = [
+  {id:1, name:'피자', price: 20000, image:'./src/assets/img-1.png',category:'Best'},
+  {id:2, name:'햄버거', price: 8000, image:'./src/assets/img-2.png',category:'세트'},
+  {id:3, name:'콜라', price: 2000, image:'./src/assets/img-3.png',category:'음료'},
+  {id:4, name:'감자튀김', price: 4000, image:'./src/assets/img-4.png',category:'디저트'},
+  {id:5, name:'밀크쉐이크', price: 3000, image:'./src/assets/img-5.png',category:'New'},
+  // {id:6, name:'어니언링', price: 3000, image:'./src/assets/img-5.png',category:'디저트'},
+  // {id:7, name:'사이다', price: 2000, image:'./src/assets/img-5.png',category:'음료'},
+  // {id:8, name:'에그타르트', price: 3000, image:'./src/assets/img-5.png',category:'디저트'},
+  // {id:9, name:'오렌지주스', price: 3000, image:'./src/assets/img-5.png',category:'음료'},
+  // {id:10, name:'치즈스틱', price: 3000, image:'./src/assets/img-5.png',category:'Best'},
+];
+export const categories = ['전체','Best','New','세트','음료','디저트'];
